3fefaad739dd9b240a2f765ab7f38dce3db5858f
m.yousefi <67848+mahdiit@users.noreply.github.com>
2022-10-10 08:53:54 +0330

    Add project files.

---
+++ SqlQueryExportExcel/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace SqlQueryExportExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkWinAthu = new System.Windows.Forms.CheckBox();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.exportDialog = new System.Windows.Forms.SaveFileDialog();
+            this.txtDb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 27);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(117, 22);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(135, 29);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(113, 22);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 74);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(190, 22);
+            this.txtIP.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 14);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Server";
+            // 
+            // chkWinAthu
+            // 
+            this.chkWinAthu.AutoSize = true;
+            this.chkWinAthu.Location = new System.Drawing.Point(209, 76);
+            this.chkWinAthu.Name = "chkWinAthu";
+            this.chkWinAthu.Size = new System.Drawing.Size(154, 18);
+            this.chkWinAthu.TabIndex = 2;
+            this.chkWinAthu.Text = "Windows Athuntication";
+            this.chkWinAthu.UseVisualStyleBackColor = true;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(12, 115);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(382, 220);
+            this.txtQuery.TabIndex = 3;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(294, 341);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(100, 23);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Export Data";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // exportDialog
+            // 
+            this.exportDialog.Filter = "Excel File|*.xlsx";
+            // 
+            // txtDb
+            // 
+            this.txtDb.Location = new System.Drawing.Point(254, 29);
+            this.txtDb.Name = "txtDb";
+            this.txtDb.Size = new System.Drawing.Size(113, 22);
+            this.txtDb.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 14);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "DB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 378);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.chkWinAthu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDb);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.txtUsername);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Export Excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkWinAthu;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.SaveFileDialog exportDialog;
+        private System.Windows.Forms.TextBox txtDb;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SqlQueryExportExcel/Form1.cs
@@ -0,0 +1,190 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlQueryExportExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+        }
+
+        private string GetConnection()
+        {
+            SqlConnectionStringBuilder conn = new SqlConnectionStringBuilder();
+            conn.InitialCatalog = txtDb.Text;
+            conn.DataSource = txtIP.Text;
+            if (chkWinAthu.Checked)
+            {
+                conn.IntegratedSecurity = true;
+            }
+            else
+            {
+                conn.Password = txtPassword.Text;
+                conn.UserID = txtUsername.Text;
+            }
+
+            return conn.ConnectionString;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            btnExport.Enabled = false;
+            Application.DoEvents();
+
+            var sqlConnection = new SqlConnection(GetConnection());
+            var sqlCommand = new SqlCommand(txtQuery.Text, sqlConnection)
+            {
+                CommandTimeout = 300
+            };
+
+            var exportFile = "";
+            if (exportDialog.ShowDialog() == DialogResult.OK)
+            {
+                exportFile = exportDialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+
+            sqlConnection.Open();
+            SqlDataReader reader = null;
+            try
+            {
+                var f = new FileInfo(exportFile);
+                ExcelPackage xlsx = new ExcelPackage(f);
+
+                reader = sqlCommand.ExecuteReader();
+                int sheetIndex = 1;
+                do
+                {
+                    int totalCount = 0;
+                    var colPos = new Dictionary<string, int>();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        colPos.Add(reader.GetName(i), i);
+                    }
+
+                    var ws = xlsx.Workbook.Worksheets.Add("Result-" + sheetIndex);
+                    ws.Cells.Style.Font.Name = "Tahoma";
+                    ws.Cells.Style.Font.Size = 9.5f;
+                    ws.Cells.Style.Numberformat.Format = "@";
+
+                    var firstHeader = ws.Cells[1, 1, 1, colPos.Count + 1];
+                    firstHeader.Merge = true;
+                    firstHeader.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    firstHeader.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                    firstHeader.Style.Font.Bold = true;
+                    firstHeader.Value = DateTime.Now.ToShortDateString();
+                    ws.Row(1).Height = 25;
+
+                    ws.Cells[2, 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    ws.Cells[2, 1].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                    ws.Cells[2, 1].Style.WrapText = false;
+                    ws.Cells[2, 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    ws.Cells[2, 1].Style.Fill.BackgroundColor.SetColor(Color.Gainsboro);
+                    ws.Cells[2, 1].Value = "No";
+
+                    while (reader.Read())
+                    {
+                        if (totalCount == 0)
+                        {
+                            int i = 1;
+                            foreach (var item in colPos)
+                            {
+                                ws.Cells[2, i + 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                                ws.Cells[2, i + 1].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                                ws.Cells[2, i + 1].Style.WrapText = false;
+                                ws.Cells[2, i + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                                ws.Cells[2, i + 1].Style.Fill.BackgroundColor.SetColor(Color.Gainsboro);
+                                ws.Cells[2, i + 1].Value = item.Key;
+                                i++;
+                            }
+                            ws.Row(2).Height = 20;
+                            ws.Cells.AutoFitColumns();
+                        }
+
+                        totalCount++;
+
+                        ws.Row(totalCount + 1).Height = 16.5;
+                        ws.Cells[totalCount + 2, 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        ws.Cells[totalCount + 2, 1].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        ws.Cells[totalCount + 2, 1].Value = totalCount;
+
+                        var j = 1; //ستون اول ستون ردیف هست
+                        foreach (var item in colPos)
+                        {
+                            var col = ws.Cells[totalCount + 2, j + 1];
+                            col.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                            var colValue = reader.GetValue(colPos[item.Key]);
+                            //if (OnRenderColumn != null)
+                            //    OnRenderColumn(item.Key, colValue, col);
+                            //else
+                            col.Value = colValue;
+                            //if (dataRead != null)
+                            //    dataRead(item.Key, colValue);
+                            j++;
+                        }
+
+                        if (totalCount % 100 == 0 && totalCount < 3000)
+                        {
+                            ws.Cells.AutoFitColumns();
+                            //OnProgress("تعداد رکوردهای پردازش شده:  " + totalCount);
+                        }
+
+                        //بیش اندازه بزرگ شده است
+                        if (totalCount == 1000000)
+                        {
+                            break;
+                        }
+                    }
+
+                    ws.Cells.AutoFitColumns();
+                    ws.View.PageLayoutView = false;
+                    //ws.View.RightToLeft = true;
+
+                    sheetIndex++;
+
+                } while (reader.NextResult());
+
+                xlsx.Save();
+                xlsx.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //clean reader
+                if (reader != null)
+                    reader.Close();
+
+                //clean command
+                sqlCommand.Dispose();
+
+                //clean connection
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+
+            btnExport.Enabled = true;
+        }
+    }
+}
